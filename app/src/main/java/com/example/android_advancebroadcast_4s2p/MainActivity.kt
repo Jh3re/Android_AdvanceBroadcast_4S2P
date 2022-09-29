c2db331bab4d5ee542ecb1de13191463a5eb7d5c
Jheremy <chamy2406@gmail.com>
2022-09-29 11:00:32 -0400

    Usando el BroadcastReceiver, con el modo avion.

--- app/src/main/java/com/example/android_advancebroadcast_4s2p/MainActivity.kt
+++ app/src/main/java/com/example/android_advancebroadcast_4s2p/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.android_advancebroadcast_4s2p
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.android_advancebroadcast_4s2p.databinding.ActivityMainBinding
@@ -7,6 +11,21 @@ import com.example.android_advancebroadcast_4s2p.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+
+    // Definir un objeto de tipo BroadcastReceiver
+    // donde directamente aplicaremos la implementacion
+    // o sobreescritura de un metodo clave en la
+    // configuracion de un Broadcast que es el metodo
+    // onReceive
+    private val getAirplaneMode = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val airplaneMode = intent?.getBooleanExtra("state", false)
+            airplaneMode?.let {
+                val message = if (it) "Modo Avion activado" else "Modo Avion desactivado"
+                binding.txtAirplaneMode.text = message
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +35,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        // Registrar el BroadcastReceiver
+        registerReceiver(getAirplaneMode, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
     }
 
     override fun onStop() {
         super.onStop()
+        // Quitar el registro
+        unregisterReceiver(getAirplaneMode)
     }
 }
