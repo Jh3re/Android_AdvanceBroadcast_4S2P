d4089cfb0d42dd0a8d25c50cb33335aefeaa9c2c
Jheremy <chamy2406@gmail.com>
2022-10-01 10:48:33 -0400

    Usando el BroadcastReceiver, con el cambio de wifi en caso de que este activado.

--- app/src/main/java/com/example/android_advancebroadcast_4s2p/MainActivity.kt
+++ app/src/main/java/com/example/android_advancebroadcast_4s2p/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
@@ -58,11 +59,37 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         tts = TextToSpeech(this, this)
     }
 
+    // Vamos a configurar el BroadcastReceiver
+    // para saber cuando el wifi esta habilitado o no
+    private val getWifiMode = object: BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            // Wifi trabaja pasando a traves de un registro en el intent
+            // Intent un dato de tipo numerico, ese dato
+            // te va a indicar aspectos como si esta habilitado,
+            // si no esta habilitado, configuraciones de la red,
+            // si el servicio esta dañado o no se reconoce
+            // Servicio Wifi: se gestiona a traves de un controlador que es una
+            // clase que lo administra -> WifiManager
+            val wifiMode = intent?.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN)
+            wifiMode?.let {
+                val message = when (it) {
+                    WifiManager.WIFI_STATE_ENABLED -> "Wifi habilitado"
+                    WifiManager.WIFI_STATE_DISABLED -> "Wifi deshabilitado"
+                    WifiManager.WIFI_STATE_UNKNOWN -> "Servicio Wifi error desconocido"
+                    else -> "No dispones de Wifi, no manches"
+                }
+                tts.speak(message, TextToSpeech.QUEUE_FLUSH, null,"")
+                binding.txtWifiMode.text = message
+            }
+        }
+    }
+
     override fun onStart() {
         super.onStart()
         // Registrar el BroadcastReceiver
         registerReceiver(getAirplaneMode, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
         registerReceiver(getTimeChange, IntentFilter(Intent.ACTION_TIME_TICK))
+        registerReceiver(getWifiMode, IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION))
     }
 
     override fun onStop() {
@@ -70,6 +97,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         // Quitar el registro
         unregisterReceiver(getAirplaneMode)
         unregisterReceiver(getTimeChange)
+        unregisterReceiver(getWifiMode)
     }
 
     override fun onInit(status: Int) {
