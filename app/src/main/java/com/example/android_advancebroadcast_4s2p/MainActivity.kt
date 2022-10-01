3b1e2ea38d37daf5b04921cfbba68d5dca3873cd
Jheremy <chamy2406@gmail.com>
2022-10-01 10:23:02 -0400

    Usando el BroadcastReceiver, con el cambio de tiempo, incluyendo un contador que con un comando de voz nos dira cuantas vaces a cambiado el tiempo desde que se inicio la aplicación.

--- app/src/main/java/com/example/android_advancebroadcast_4s2p/MainActivity.kt
+++ app/src/main/java/com/example/android_advancebroadcast_4s2p/MainActivity.kt
@@ -6,11 +6,16 @@ import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
 import com.example.android_advancebroadcast_4s2p.databinding.ActivityMainBinding
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var tts: TextToSpeech
+    private var c = 0
 
     // Definir un objeto de tipo BroadcastReceiver
     // donde directamente aplicaremos la implementacion
@@ -21,7 +26,8 @@ class MainActivity : AppCompatActivity() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val airplaneMode = intent?.getBooleanExtra("state", false)
             airplaneMode?.let {
-                val message = if (it) "Modo Avion activado" else "Modo Avion desactivado"
+                val message = if (it) "Modo Avión activado"  else "Modo Avión desactivado"
+                tts.speak(message, TextToSpeech.QUEUE_FLUSH, null,"")
                 binding.txtAirplaneMode.text = message
             }
         }
@@ -31,7 +37,17 @@ class MainActivity : AppCompatActivity() {
     // Time Tick
     private val getTimeChange = object: BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            binding.txtTimeTick.text = "El tiempo ha cambiado"
+            c++
+            if(c == 1){
+                val message = "El tiempo ha cambiado $c vez"
+                tts.speak(message, TextToSpeech.QUEUE_FLUSH, null,"")
+                binding.txtTimeTick.text = message
+            }else{
+                val message = "El tiempo ha cambiado $c veces"
+                tts.speak(message, TextToSpeech.QUEUE_FLUSH, null,"")
+                binding.txtTimeTick.text = message
+            }
+
         }
     }
 
@@ -39,6 +55,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        tts = TextToSpeech(this, this)
     }
 
     override fun onStart() {
@@ -53,5 +70,13 @@ class MainActivity : AppCompatActivity() {
         // Quitar el registro
         unregisterReceiver(getAirplaneMode)
         unregisterReceiver(getTimeChange)
+    }
+
+    override fun onInit(status: Int) {
+        val result = if (status == TextToSpeech.SUCCESS) {
+            tts.language = Locale("es")
+            "Suba el volumen para el uso de voz"
+        } else "Algo salio mal."
+        Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
     }
 }
