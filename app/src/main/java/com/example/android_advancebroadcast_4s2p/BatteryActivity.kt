ac98121f88b0a436159b1c2e3b0f925d6bf86c96
Jheremy <chamy2406@gmail.com>
2022-10-12 20:40:19 -0400

    Con la Clase MyBroadcast vemos el porcentaje de bateria, y tambien si en caso esta bajo.

--- app/src/main/java/com/example/android_advancebroadcast_4s2p/BatteryActivity.kt
+++ app/src/main/java/com/example/android_advancebroadcast_4s2p/BatteryActivity.kt
@@ -1,11 +1,30 @@
 package com.example.android_advancebroadcast_4s2p
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_advancebroadcast_4s2p.databinding.ActivityBatteryBinding
 
 class BatteryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBatteryBinding
+    private lateinit var myBroadcast: MyBroadcast
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_battery)
+        binding = ActivityBatteryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        myBroadcast = MyBroadcast(binding)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(myBroadcast, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        registerReceiver(myBroadcast, IntentFilter(Intent.ACTION_BATTERY_LOW))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(myBroadcast)
     }
 }
--- app/src/main/java/com/example/android_advancebroadcast_4s2p/MyBroadcast.kt
+++ app/src/main/java/com/example/android_advancebroadcast_4s2p/MyBroadcast.kt
@@ -4,18 +4,40 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.BatteryManager
+import com.example.android_advancebroadcast_4s2p.databinding.ActivityBatteryBinding
 
 // Para que esta clase se comporte como un broadcastReceiver hay que extender una clase abstracta,
 // usado 3 veces en el ejemplo
-class MyBroadcast: BroadcastReceiver() {
+class MyBroadcast(
+    private val bindingObject: ActivityBatteryBinding
+): BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        TODO("Configurar nivel de bateria")
         when(intent?.action){
             Intent.ACTION_BATTERY_CHANGED -> showBatteryLevel(intent)
+            Intent.ACTION_BATTERY_LOW -> configureBatteryLow(intent)
         }
     }
 
-    private fun showBatteryLevel(intent: Intent) {
+    private fun configureBatteryLow(intent: Intent) {
+        // El dato que les llega a travees del archivo Extras del Intent
+        // es un booleano
+        var batterylow = intent?.getBooleanExtra(BatteryManager.EXTRA_BATTERY_LOW,false)
+        batterylow?.let {
+            bindingObject.txtBatteryMessage.text = "Bateria baja"
+        }
 
+
+    }
+
+    private fun showBatteryLevel(intent: Intent) {
+        // Un valor entero que llega en extras del Intent
+        // que el valor del nivel de la bateria
+        // La bateria tiene un gestor llamado BatteryManager
+        val batteryLevel = intent?.getIntExtra(BatteryManager.EXTRA_LEVEL,0)
+        batteryLevel?.let {
+            val message = "Porcentaje de bateria: $it%"
+            bindingObject.txtBatteryPercentage.text = message
+            bindingObject.pbBatteryLevel.progress = it
+        }
     }
 }
